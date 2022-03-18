b96da7593244ddc5e022efc10f185a5b01fe2b6b
mitesh pandey <excelrock_mitesh@yahoo.com>
2022-03-18 19:35:55 +0545

    final code completed till course end

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ mod http;
 use server::Server;
 use website_handler::WebsiteHandler;
 mod website_handler;
+use std::env;
+//It makes sense to path the path to public folder as the environment variable
+//So for that, we make use of the environment variables in rust
 
 
 fn main() {
-   
+    let default_path = format!("{}/public",env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);//If PUBLIC_PATH is not passed as environment variable while cargo run, then it will take the default_path. 
     let server = Server :: new(String::from("127.0.0.1:8080") );
-    server.run(WebsiteHandler);//We dont have to pass any parameter because it is empty struct
+    server.run(WebsiteHandler::new(public_path));//We dont have to pass any parameter because it is empty struct
 }
 
 
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,54 @@
 use super::server::Handler;
-use super::http::{Request,Response,StatusCode};
-pub struct WebsiteHandler;
+use super::http::{Request,Response,StatusCode,Method};
+use std::fs;
+pub struct WebsiteHandler{
+    public_path:String
+}
+
+//All the routing logic to specific request based on the path will be implemented here
+
+impl WebsiteHandler{
+    pub fn new(public_path:String) -> Self{
+        Self{
+            public_path
+        }
+    }
+
+    fn read_file(&self,file_path : &str) -> Option<String>{
+
+        //To prevent the directory traversal vulnerability, we will calculate the absolute canonical path and check whether that starts with public or not
+
+        let path = format!("{}/{}",self.public_path,file_path);
+        match fs::canonicalize(path){
+            Ok(path) => {
+                if path.starts_with(&self.public_path){
+                    fs::read_to_string(path).ok()
+                }else {
+                    println!("Directory Traversal Attack Attempted : {}",file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
+        //ok() is very handly method for conversion between Result<T,E> to Option<>.
+        // fs::read_to_string(path).ok() 
+    }
+}
 
 
 impl Handler for WebsiteHandler{
     fn handle_request(&mut self,request:&Request) -> Response {
-         Response::new(StatusCode::Ok,Some("<h1>ThIS WORKS<h1>".to_string()))
+         match request.method(){
+            Method::GET => match request.path(){
+                "/" => Response::new(StatusCode::Ok,self.read_file("index.html")),
+                path => match self.read_file(path){ //This is the terrible security vulnerability, directory traversal vulnerability
+                    Some(contents) => Response::new(StatusCode::Ok,Some(contents)),
+                    None => Response::new(StatusCode::NotFound,None)
+
+                },
+                
+            } ,
+            _ => Response::new(StatusCode::NotFound,None)
+         }
     }
 }
